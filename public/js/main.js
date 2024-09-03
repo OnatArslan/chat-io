b7f4a97a73053787a1464729217a6b8fb1550098
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-03 05:23:13 +0300

    Commit for changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,16 @@
+const chatForm = document.getElementById(`chat-form`);
+
 const socket = io();
 
 socket.on(`message`, (message) => {
   console.log(message);
+});
+
+// Message submit
+chatForm.addEventListener(`submit`, (event) => {
+  event.preventDefault();
+
+  const msg = event.target.elements.msg.value;
+
+  socket.emit(`chatMessage`, msg);
 });
